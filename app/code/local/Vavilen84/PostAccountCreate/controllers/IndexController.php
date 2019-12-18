fe0525da9b28ce05f709cf6466879b4b91d04d6a
Vladimir Teplov <vteplov@brightgrove.com>
2019-12-18 15:21:07 +0200

    add post account creation module

--- app/code/local/Vavilen84/PostAccountCreate/controllers/IndexController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Vavilen84_PostAccountCreate_IndexController extends Mage_Core_Controller_Front_Action
-{
-    public function indexAction()
-    {
-        $this->loadLayout()->renderLayout();
-    }
-}
