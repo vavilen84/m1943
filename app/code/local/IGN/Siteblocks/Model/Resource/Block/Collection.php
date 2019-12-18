1a59229bfb26356fbafde67a8c9a43ee4de2c9fc
Vladimir Teplov <vteplov@brightgrove.com>
2019-12-18 14:18:09 +0200

    add helloworld module

--- app/code/local/IGN/Siteblocks/Model/Resource/Block/Collection.php
+++
@@ -1,9 +0,0 @@
-<?php
-class IGN_Siteblocks_Model_Resource_Block_Collection extends Mage_Core_Model_Mysql4_Collection_Abstract
-{
-    public function __construct()
-    {
-        parent::__construct();
-        $this->_init('siteblocks/block');
-    }
-}
---
+++ app/code/local/Vavilen84/PostAccountCreate/Block/Data.php
@@ -0,0 +1,14 @@
+<?php
+
+class Vavilen84_PostAccountCreate_Block_Data extends Mage_Core_Block_Template
+{
+    public function __construct(array $args = array())
+    {
+        parent::__construct($args);
+    }
+
+    public function e()
+    {
+        return "test";
+    }
+}
