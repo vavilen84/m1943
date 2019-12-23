b2141b4fa9960c409647609122732b8e5167b75b
Vladimir Teplov <vteplov@brightgrove.com>
2019-12-23 18:16:59 +0200

    IGN: add admin grid

---
+++ app/code/local/IGN/Siteblocks/Block/Adminhtml/Siteblocks.php
@@ -0,0 +1,25 @@
+<?php
+
+class IGN_Siteblocks_Block_Adminhtml_Siteblocks extends Mage_Adminhtml_Block_Widget_Grid_Container
+{
+    public function __construct()
+    {
+        $this->_controller = 'adminhtml_siteblocks';
+        $this->_blockGroup = 'siteblocks';
+        $this->_headerText = Mage::helper('siteblocks')->__('Siteblocks');
+
+        parent::__construct();
+
+        if ($this->_isAllowedAction('save')) {
+            $this->_updateButton('add', 'label', Mage::helper('siteblocks')->__('Add New Block'));
+        } else {
+            $this->_removeButton('add');
+        }
+    }
+
+    protected function _isAllowedAction($action)
+    {
+       // return Mage::getSingleton('admin/session')->isAllowed('cms/page/' . $action);
+        return true;
+    }
+}
---
+++ app/code/local/IGN/Siteblocks/controllers/Adminhtml/SiteblocksController.php
@@ -0,0 +1,8 @@
+<?php
+class IGN_Siteblocks_Adminhtml_SiteblocksController extends Mage_Adminhtml_Controller_Action
+{
+    public function indexAction()
+    {
+        $this->loadLayout()->renderLayout();
+    }
+}
