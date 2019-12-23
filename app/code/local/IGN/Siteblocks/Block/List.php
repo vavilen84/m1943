375b60548bd94a2085a3a315941717ec2d420233
Vladimir Teplov <vteplov@brightgrove.com>
2019-12-23 15:53:05 +0200

    IGN: add block/layout/template

--- app/code/local/IGN/Siteblocks/Block/List.php
+++ app/code/local/IGN/Siteblocks/Block/List.php
@@ -1,5 +1,5 @@
 <?php
 class IGN_Siteblocks_Block_List extends Mage_Core_Block_Template
 {
-
+    public $count = 5;
 }
--- app/code/local/IGN/Siteblocks/controllers/IndexController.php
+++ app/code/local/IGN/Siteblocks/controllers/IndexController.php
@@ -1,12 +1,11 @@
 <?php
 class IGN_Siteblocks_IndexController extends Mage_Core_Controller_Front_Action
 {
-    public $count;
-
     public function indexAction()
     {
         $count = Mage::getStoreConfig('siteblocks/general/blocks_count', Mage::app()->getStore());
         Mage::app()->getConfig()->saveConfig('siteblocks/general/blocks_count', 5);
-        $this->count = $count;
+
+        $this->loadLayout()->renderLayout();
     }
 }
