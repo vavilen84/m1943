dac3f1a1243bdc719bf423ad6895f88a026afe04
Vladimir Teplov <vteplov@brightgrove.com>
2019-12-23 15:24:44 +0200

    IGN: save config value from controller

--- app/code/local/IGN/Siteblocks/controllers/IndexController.php
+++ app/code/local/IGN/Siteblocks/controllers/IndexController.php
@@ -3,7 +3,8 @@ class IGN_Siteblocks_IndexController extends Mage_Core_Controller_Front_Action
 {
     public function indexAction()
     {
-        $isEnabled = Mage::getStoreConfig('siteblocks/general/blocks_count', Mage::app()->getStore());
-        var_dump($isEnabled);die;
+        $count = Mage::getStoreConfig('siteblocks/general/blocks_count', Mage::app()->getStore());
+        Mage::app()->getConfig()->saveConfig('siteblocks/general/blocks_count', 5);
+        var_dump($count);die;
     }
 }
