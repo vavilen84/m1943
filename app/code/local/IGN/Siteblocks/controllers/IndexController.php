9590a674f57cf298afcd891b07997676cd421cc7
Vladimir Teplov <vteplov@brightgrove.com>
2019-12-23 15:21:22 +0200

    IGN: get config value

--- app/code/local/IGN/Siteblocks/controllers/IndexController.php
+++ app/code/local/IGN/Siteblocks/controllers/IndexController.php
@@ -3,6 +3,7 @@ class IGN_Siteblocks_IndexController extends Mage_Core_Controller_Front_Action
 {
     public function indexAction()
     {
-        echo 123;die;
+        $isEnabled = Mage::getStoreConfig('siteblocks/general/blocks_count', Mage::app()->getStore());
+        var_dump($isEnabled);die;
     }
 }
